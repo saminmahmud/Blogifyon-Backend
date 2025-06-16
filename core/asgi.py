1223fef1bc823814e20fa05a9510ac6f17a5aeec
saminmahmud <samin.mahmud18@gmail.com>
2025-06-17 01:19:13 +0600

    change import module position in asgi.py

--- core/asgi.py
+++ core/asgi.py
@@ -1,17 +1,16 @@
-
 import os
-import django
 
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from notification.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+import django
 django.setup()
 
+from notification.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
